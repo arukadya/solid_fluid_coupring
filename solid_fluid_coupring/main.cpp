3338a51873ff612c52dbe0570f53445c9ece3d91
arukadya <toshiki.arukadya@outlook.jp>
2024-06-30 20:44:17 +0900

    Initial Commit

---
+++ solid_fluid_coupring/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  solid_fluid_coupring
+//
+//  Created by 須之内俊樹 on 2024/06/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
